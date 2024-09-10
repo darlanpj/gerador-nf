7756fd7503f1798c48ca7f57791624e209a33039
djambersi <djambersi@uolinc.com>
2024-09-09 23:01:19 -0300

    fix test

--- src/main/java/br/com/itau/geradornotafiscal/service/CalculadoraAliquotaProduto.java
+++ src/main/java/br/com/itau/geradornotafiscal/service/CalculadoraAliquotaProduto.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalculadoraAliquotaProduto {
-    private static List<ItemNotaFiscal> itemNotaFiscalList = new ArrayList<>();
+    private List<ItemNotaFiscal> itemNotaFiscalList = new ArrayList<>();
 
     public List<ItemNotaFiscal> calcularAliquota(List<Item> items, double aliquotaPercentual) {
 
